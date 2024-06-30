1bc7a8a0705c63ec31a56912ad13e09045e81536
Tran Danh <TranTrongDanh15@gmail.com>
2024-06-30 16:24:07 +0700

    nothing

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,33 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
-import envCompatible from "vite-plugin-env-compatible";
-import dotenv from "dotenv";
 
-// Load environment variables from .env file
-dotenv.config();
-console.log(process.env.VITE_API_BASE_URL);
-console.log(process.env);
-console.log(import.meta.env);
-export default defineConfig({
-  plugins: [react(), envCompatible()],
-  define: {
-    "process.env": process.env, // Add this line to define environment variables
-  },
-  server: {
-    proxy: {
-      "/foo": "http://localhost:5173/",
-      "/api": {
-        target: process.env.VITE_API_BASE_URL,
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, ""),
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  // Load environment variables based on the current mode (development, production, etc.)
+  const env = loadEnv(mode, process.cwd(), "");
+
+  console.log(env.VITE_API_BASE_URL);
+
+  return {
+    plugins: [react()],
+    define: {
+      "process.env": {
+        VITE_API_BASE_URL: JSON.stringify(env.VITE_API_BASE_URL),
       },
     },
-  },
+    server: {
+      proxy: {
+        "/foo": "http://localhost:5173/",
+        "/api": {
+          target: env.VITE_API_BASE_URL,
+          changeOrigin: true,
+          secure: false,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
+      },
+    },
+    build: {
+      outDir: "dist",
+    },
+  };
 });
