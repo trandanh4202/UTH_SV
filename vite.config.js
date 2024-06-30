f56b142916ab4b04011c3b6efb854cb7845cd418
Tran Danh <TranTrongDanh15@gmail.com>
2024-06-30 15:42:46 +0700

    nothing

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
 export default defineConfig({
   plugins: [react()],
+  // define: {
+  //   "process.env.VITE_API_BASE_URL": JSON.stringify(
+  //     process.env.VITE_API_BASE_URL
+  //   ),
+  // },
   server: {
     proxy: {
       "/foo": "http://localhost:5173/",
       "/api": {
-        target: import.meta.env.VITE_API_BASE_URL,
+        target: process.env.VITE_API_BASE_URL,
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, ""),
